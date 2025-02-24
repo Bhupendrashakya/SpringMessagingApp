981e6175b6c898e74bc084b72f42781be1f1a749
Bhupendra shakya <bhupendrashakya714@gmail.com>
2025-02-24 15:54:01 +0530

    [Bhupendra Shakya] Add. Rest Controller to demonstrate the various HTTP Methods and respond hello messages to the User.

---
+++ src/main/java/com/Spring/SpringMessagingApp/controller/HelloRestController.java
@@ -0,0 +1,17 @@
+package com.Spring.SpringMessagingApp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+
+    // curl localhost:8080/hello
+    @RequestMapping(value = {"", "/", "/home"})
+    public String sayHello() {
+        return "Hello From BridgeLabz!!!";
+    }
+
+
+}
